ab730a2d8787b23c37005d9771a8652acc873054
Joshua Flack <joshu.alexander@gmail.com>
2017-06-13 15:34:11 -0400

    readability

--- scripts/main.js
+++ scripts/main.js
@@ -14,56 +14,25 @@ let testHand3 = ["7", "A", "5"]
 
 function handValue(hand) {
   let handTotal = 0;
-  // let aceBucket = [];
-  // for (x = 0; x < hand.length; x++) {
-  //   if (hand[x] === "A")  {
-  //     aceBucket.push(hand[x]);
-  //     console.log(aceBucket);
-  //     hand.splice(hand[x], 1);
-  //   }
-  // }
-  // hand = hand.concat(aceBucket);
-  // console.log(hand);
   for (x = 0; x < hand.length; x++) {
-    if (hand[x] === "J" || hand[x] === "Q" || hand[x] ===  "K") {
+    if (hand[x] === "J" || hand[x] === "Q" || hand[x] === "K") {
       handTotal += 10;
-    // } else if (hand[x] === "A") {
-    //   if (handTotal + 11 <= 21) {
-    //     handTotal += 11;
-    //   }
-    // else {
-    //     handTotal += 1;
-    //   }
-  } else if (hand[x] === "A") {
-
-  } else {
+    } else if (hand[x] === "A") {} else {
       handTotal += Number(hand[x]);
     }
   }
-
   for (x = 0; x < hand.length; x++) {
     if (hand[x] === "A" && handTotal <= 10) {
       handTotal += 11;
-    } else if (hand[x] ==="A" && handTotal > 10) {
+    } else if (hand[x] === "A" && handTotal > 10) {
       handTotal += 1;
     }
   }
-
-  // if (handTotal > 21) {
-  //   handTotal -= 10;
-  // }
   return handTotal;
 }
-
 console.log(handValue(testHand1));
 console.log(handValue(testHand2));
 console.log(handValue(testHand3));
-
-
-
-
-
-
 
 /* -----  Hints ------
 
